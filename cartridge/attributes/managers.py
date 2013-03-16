fdc2b23a05fd5ff297d138e1acfc1e1c143da176
wrwrwr <git@wr.waw.pl>
2013-03-16 17:18:42 +0100

    Also made choice attributes polymorphic -- it's needed to easily compare attribute pointed to by option with a specialized subclass instance.

--- cartridge/attributes/managers.py
+++ cartridge/attributes/managers.py
@@ -4,7 +4,7 @@ from django.db import models
 class PolymorphicQuerySet(models.query.QuerySet):
     def iterator(self):
         for obj in super(PolymorphicQuerySet, self).iterator():
-            yield obj.content_type.get_object_for_this_type(id=obj.id)
+            yield obj.as_content_type()
 
 
 class PolymorphicManager(models.Manager):
