9b961d19deba2c1f0cae79746cef66a2a0353af3
wrwrwr <git@wr.waw.pl>
2013-03-30 12:46:05 +0100

    Implemented loyalty discounts, a discount flavour taking into consideration former orders of a customer, but not requiring entering a code.

---
+++ cartridge/shop/translation.py
@@ -0,0 +1,39 @@
+
+from cartridge.shop.models import (
+    Product, ProductImage, ProductOption, Category, SelectedProduct, CartItem,
+    OrderItem, Discount, Sale, DiscountCode, LoyaltyDiscount)
+
+from modeltranslation.translator import TranslationOptions, translator
+
+
+class ProductTranslationOptions(TranslationOptions):
+    fields = ("keywords_string",)
+
+
+class ProductImageTranslationOptions(TranslationOptions):
+    fields = ("description",)
+
+
+class ProductOptionTranslationOptions(TranslationOptions):
+    fields = ("name",)
+
+
+class SelectedProductTranslationOptions(TranslationOptions):
+    fields = ("description",)
+
+
+class CartItemTranslationOptions(TranslationOptions):
+    fields = ("url",)
+
+
+class DiscountTranslationOptions(TranslationOptions):
+    fields = ("title",)
+
+
+translator.register(Product, ProductTranslationOptions)
+translator.register(ProductImage, ProductImageTranslationOptions)
+translator.register(ProductOption, ProductOptionTranslationOptions)
+translator.register(SelectedProduct, SelectedProductTranslationOptions)
+translator.register(CartItem, CartItemTranslationOptions)
+translator.register(Discount, DiscountTranslationOptions)
+translator.register((Category, OrderItem, Sale, DiscountCode, LoyaltyDiscount))
