40abf001b2e7fe206f4805799634b474a9908c93
wrwrwr <git@wr.waw.pl>
2013-04-18 14:14:57 +0200

    Separated cart updating from the cart view, so some fancy cart logic, like saving the cart and returning or saving and proceeding to checkout, may be implemented conveniently.

--- cartridge/shop/utils.py
+++ cartridge/shop/utils.py
@@ -74,6 +74,19 @@ def recalculate_discount(request):
             pass
 
 
+def update_cart(request):
+    """
+    If the cart formset is valid saves it and returns ``True``.
+    """
+    from cartridge.shop.forms import CartItemFormSet
+    cart_formset = CartItemFormSet(request.POST, instance=request.cart)
+    valid = cart_formset.is_valid()
+    if valid:
+        cart_formset.save()
+        recalculate_discount(request)
+    return valid
+
+
 def set_shipping(request, shipping_type, shipping_total):
     """
     Stores the shipping type and total in the session.
